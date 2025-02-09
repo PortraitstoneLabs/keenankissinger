d0cc2eb57238c218d31cc1accc83aafa13dc8d80
khristophekeen <khristophe.keen@gmail.com>
2025-02-09 14:49:30 -0800

    Added YouTube video

--- src/components/YouTubeVideo.jsx
+++ src/components/YouTubeVideo.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
-import YouTube from 'react-youtube';
 
 const YouTubeVideo = () => {
-  const videoId = '_s0KaPfkmnY';
+  // Define both video IDs
+  const videoIds = ['dYleXm-l6yc', '_s0KaPfkmnY'];
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100%', maxWidth: '100%', overflow: 'hidden' }}>
-    <div style={{ width: '100%', maxWidth: '560px' }}>
-      <div className="video-wrapper">
-        <iframe 
-          title="YouTube Video 1"
-          width="100%" 
-          height="315" 
-          src={`https://www.youtube-nocookie.com/embed/${videoId}`}  // Using YouTube-nocookie domain
-          frameBorder="0" 
-          allowFullScreen
+      
+      <div style={{ width: '100%', maxWidth: '560px', marginBottom: '20px' }}>
+        <div className="video-wrapper">
+          <iframe 
+            title="YouTube Video 1"
+            width="100%" 
+            height="315" 
+            src={`https://www.youtube-nocookie.com/embed/${videoIds[0]}`}  // First video ID
+            frameBorder="0" 
+            allowFullScreen
           ></iframe>
         </div>
       </div>
+      
+      <div style={{ width: '100%', maxWidth: '560px' }}>
+        <div className="video-wrapper">
+          <iframe 
+            title="YouTube Video 2"
+            width="100%" 
+            height="315" 
+            src={`https://www.youtube-nocookie.com/embed/${videoIds[1]}`}  // Second video ID
+            frameBorder="0" 
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+
     </div>
   );
 }
